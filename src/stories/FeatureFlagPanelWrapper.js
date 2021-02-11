20d0f117698ad661cb5e4320aa448e9812d09dd2
Can Cubukcu <can@jotform.com>
2021-02-11 22:40:19 +0300

    Story :: Feature Flag Panel Story updated

--- src/stories/FeatureFlagPanelWrapper.js
+++ src/stories/FeatureFlagPanelWrapper.js
@@ -2,17 +2,43 @@ import React, { useEffect, useState } from 'react';
 import FeatureFlagPanel from '../FeatureFlag/FeatureFlagPanel';
 import { extractFeatureFlagsFromConfig } from '../FeatureFlag/FeatureFlag';
 
+const getFeatureValue = (feature, isChecked) => {
+  if (!isChecked) return false;
+  return feature.options ? feature.options[0].value : true;
+}
+
 export const FeatureFlagPanelWrapper = ({ config: _config }) => {
   const [config, setConfig] = useState(_config);
   useEffect(() => {
     setConfig(_config);
   }, [_config])
-  const flags = extractFeatureFlagsFromConfig(config);
+
+  const handleOptionValueChange = (id) => (value) => (e) => {
+    const feature = config.find(feat => feat.id === id);
+    const featureIndex = config.findIndex(feat => feat.id === id);
+    const __config = [...config.slice(0,featureIndex), {...feature, defaultValue: value }, ...config.slice(featureIndex + 1, config.length)]
+    setConfig(__config);
+  };
+
+  const handleFeatureToggleChange = (id) => (e) => {
+    const isChecked = e.currentTarget.checked;
+
+    const feature = config.find(feat => feat.id === id);
+    const featureIndex = config.findIndex(feat => feat.id === id);
+    const featureValue = getFeatureValue(feature, isChecked);
+
+    const __config = [...config.slice(0,featureIndex), {...feature, defaultValue: featureValue }, ...config.slice(featureIndex + 1, config.length)]
+    setConfig(__config);
+  };
+
+  const flags = extractFeatureFlagsFromConfig(config, false);
 
   return (
     <FeatureFlagPanel
       config={config}
       flags={flags}
+      handleOptionValueChange={handleOptionValueChange}
+      handleFeatureToggleChange={handleFeatureToggleChange}
     />
   )
 }
